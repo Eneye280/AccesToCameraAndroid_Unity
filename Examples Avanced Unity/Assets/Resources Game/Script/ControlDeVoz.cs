17d68dae5b8ac4af6a23d659680336f6ed014511
Martinez <eneye280@gmail.com>
2018-11-05 19:00:42 -0600

    Start project

---
+++ Examples Avanced Unity/Assets/Resources Game/Script/ControlDeVoz.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class ControlDeVoz : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keyworks = new Dictionary<string, System.Action>();
+
+    private void Start()
+    {
+        keyworks.Add("photo", () =>
+        {
+            GoCalled();
+        });
+
+        keywordRecognizer = new KeywordRecognizer(keyworks.Keys.ToArray());
+		keywordRecognizer.OnPhraseRecognized += KeywordRecognizerOnPhrase;
+		keywordRecognizer.Start();
+    }
+	private void KeywordRecognizerOnPhrase(PhraseRecognizedEventArgs args)
+	{
+		System.Action keyworksAction;
+
+		if(keyworks.TryGetValue(args.text,out keyworksAction))
+		{
+			keyworksAction.Invoke();
+		}
+	}
+    private void GoCalled()
+    {
+		print("Hola euclides ");
+    }
+}
